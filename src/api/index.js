d00e86104e4dc8d397ce0c33a54e9f6ea38b83f3
Qiang Yiting <suntoe@189.cn>
2017-11-26 08:04:54 +0800

    0.2.23

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,6 @@
 module.exports = {
 
     UserAPI: require('./UserAPI').default,
-    AdminAPI: require('./AdminAPI').default,
-    RootAPI: require('./RootAPI').default,
     OrgUserAPI: require('./OrgUserAPI').default,
     OrgAdminAPI: require('./OrgAdminAPI').default
 
