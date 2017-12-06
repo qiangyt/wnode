9fd0c03c0c135469cf1013bf761374eec17c05ce
Qiang Yiting <suntoe@189.cn>
2017-12-06 13:28:28 +0800

    0.2.39

--- src/util/Misc.ts
+++ src/util/Misc.ts
@@ -3,3 +3,7 @@ import * as NodeUuid from 'node-uuid';
 export function uuid():string {
     return NodeUuid.v4();
 }
+
+export function prettyJson(obj:any):string {
+    return JSON.stringify(obj, null, 4);
+}
