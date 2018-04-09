440cb973cf7e77af89cca3251ee9919c5a2db74e
Qiang Yiting <suntoe@189.cn>
2018-04-10 04:58:21 +0800

    0.2.71

--- src/SupportedMIME.ts
+++ src/SupportedMIME.ts
@@ -1,6 +1,9 @@
 export default [
     'application/json',
     'application/octet-stream',
+    'application/pdf',
+    'application/zip',
+    'application/x-gzip',
     'image/jpeg',
     'image/bmp',
     'text/markdown',
