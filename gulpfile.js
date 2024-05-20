419487a68c3882fa5f53be3c0350e47ad4d1a090
Yiting Qiang <qiangyt@wxcount.com>
2024-05-20 16:05:14 +0800

    upgrade dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,32 @@ var tsProject = ts.createProject("tsconfig.json");
 var sourcemaps = require('gulp-sourcemaps');
 var merge2 = require('merge2');
 var fs = require('fs');
-var rimraf = require('rimraf');
+var rimraf  = require('rimraf').rimraf;
 
 
-gulp.task("clean", function() {
-    rimraf('dist', () => {});
+gulp.task("clean", async function() {
+    await rimraf('dist');
 });
 
 gulp.task("default", function() {
-    var tsResult = tsProject.src().pipe(tsProject());
+    var tsResult = tsProject.src()
+                    .pipe(sourcemaps.init())
+                    .pipe(tsProject());
 
     gulp.src(['./src/**/*.js', './src/**/*.json'])
         .pipe(gulp.dest('dist'));
 
     return merge2([ // Merge the two output streams, so this task is finished when the IO of both operations is done.
         tsResult.dts.pipe(gulp.dest('dist')),
-        tsResult.js.pipe(gulp.dest('dist'))
+        tsResult.js
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest('dist'))
     ]);
 });
 
+gulp.task('build', gulp.series('clean', 'default'));
 
-gulp.task('watch', ['default'], function() {
-    gulp.watch('src/**/*.ts', ['default']);
-});
+
+//gulp.task('watch', ['default'], function() {
+//    gulp.watch('src/**/*.ts', ['default']);
+//});
